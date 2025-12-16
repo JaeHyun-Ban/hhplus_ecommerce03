252954ee3b5cd67afcb54c2dc3f43ed62421d633
JaeHyun-Ban <wogus0008@gmail.com>
2025-12-16 13:35:47 +0900

    update: 결제도메인 분리

--- src/main/java/com/hhplus/ecommerce/order/domain/PaymentMethod.java
+++
@@ -1,5 +0,0 @@
-package com.hhplus.ecommerce.order.domain;
-
-public enum PaymentMethod {
-    BALANCE  // 잔액 결제
-}
--- src/main/java/com/hhplus/ecommerce/order/domain/PaymentStatus.java
+++
@@ -1,8 +0,0 @@
-package com.hhplus.ecommerce.order.domain;
-
-public enum PaymentStatus {
-    PENDING,    // 대기 중
-    COMPLETED,  // 완료
-    FAILED,     // 실패
-    CANCELLED   // 취소
-}
